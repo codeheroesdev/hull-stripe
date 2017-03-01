a15f1644849822401605ec294c70b11649ece78c
Michal Raczka <michal@raczka.me>
2017-03-01 16:47:00 +0100

    Fix id only user resolution

--- server/lib/get-user-ident.js
+++ server/lib/get-user-ident.js
@@ -1,21 +1,26 @@
 import _ from "lodash";
 
+/**
+ * Build an argument for `hull.as()` method.
+ * @param  {Object} ctx      Context object
+ * @param  {Object} customer [description]
+ * @return {mixed} object { external_id, anonymous_id } or { email, anonymous_id }
+ */
 export default function getUserIdent(ctx, customer) {
-  const matchingParam = _.get(ctx, "ship.private_settings.metadata_external_id_parameter");
+  const matchingParam = _.get(ctx, "ship.private_settings.metadata_id_parameter");
+  const idName = _.get(ctx, "ship.private_settings.id_parameter");
 
   const ident = {
     anonymous_id: `stripe:${customer.id}`
   };
 
   if (_.get(customer.metadata, matchingParam)) {
-    _.merge(ident, {
-      external_id: _.get(customer.metadata, matchingParam)
-    });
+    if(idName === "id") {
+      return _.get(customer.metadata, matchingParam);
+    }
+    ident[idName] = _.get(customer.metadata, matchingParam);
   } else {
-    _.merge(ident, {
-      email: customer.email
-    });
+    ident.email = customer.email;
   }
-  console.log("IDENT", ident);
   return ident;
 }
