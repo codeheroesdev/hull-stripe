b24d845d83c36588180ee65146d6715c2dfc2435
Michal Raczka <michal@raczka.me>
2017-03-03 10:09:29 +0100

    Adjust logging to match other connectors

--- server/lib/store-event.js
+++ server/lib/store-event.js
@@ -7,7 +7,7 @@ export default function storeEvent({ user, event, name, hull }) {
 
   // Only track if we support this event type
   if (name) {
-    hull.logger.info("event.store", { name, properties, context, ...user });
+    hull.logger.info("incoming.event", { name, properties, context, ...user });
     return hull.as(user).track(name, properties, context);
   }
   hull.logger.warn("event.skip", { name, context, ...user });
